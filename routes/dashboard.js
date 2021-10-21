9c41764d96f12ed38d709cdac6bfae2f2bcd2c9d
nicoleasf <61835942+nicoleasf@users.noreply.github.com>
2021-10-21 12:03:56 -0300

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,22 @@
 module.exports= (app)=>{
+    var conexao = require("../config/database.js")()
+    var usuarios = require("../models/usuarios")
 
-    app.get('/dashboard', (req,res)=>{
+    app.get('/dashboard', async (req,res)=>{
         var id = req.query.id
         console.log(id)
         if(!id){
             res.redirect('/login')
-        }
-        res.render('dashboard.ejs', {login:{nome:'teste', _id:'teste'}})
-    })
+        }else{
+            var userexiste = await usuarios .findOne({_id:id})
+            if(userexiste){
+                 res.render('dashboard.ejs', {login:userexiste})
+            }else{
+                res.redirect("/login)
+                             
+                
+      )      
+            }  
+        })
 
 }
