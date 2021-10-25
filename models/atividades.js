7d1bcd837a80867afa883cd0178187ba368896f8
nicoleasf <61835942+nicoleasf@users.noreply.github.com>
2021-10-25 11:13:19 -0300

    Create atividades.js

---
+++ models/atividades.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+var modelo = new mongoose.Schema({
+    data:Data,
+    tipo:String,
+    entrega:String,
+    disciplina:String,
+    instrucoes:String
+})
+
+module.exports = mongoose.model('usuarios',modelo)
