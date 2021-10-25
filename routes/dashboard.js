be68b7176b7a37c9f76d417e5a72f0ef34abc35b
nicoleasf <61835942+nicoleasf@users.noreply.github.com>
2021-10-25 11:44:35 -0300

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,8 @@
 module.exports= (app)=>{
     var conexao = require("../config/database.js")()
     var usuarios = require("../models/usuarios")
-
+    var atividades = require ("../models/atividades")
+    
     app.get('/dashboard', async (req,res)=>{
         var id = req.query.id
         console.log(id)
@@ -18,5 +19,23 @@ module.exports= (app)=>{
             }     
         }  
         })
-
+        
+    app.post('/dashboard',async(req,res)=>{
+    
+        var dados = req.body
+        
+        var novo = await new atividades({
+            data: dados.data,
+            tipo: dados.tipo,
+            entrega: dados.entrega,
+            disciplina: dados.disciplina,
+            instrucoes: dados.instrucoes,
+            user: dados.id
+         }).save()
+        .the,((result)=>{
+            res.redirect('/dashboard?id= '+dados.id)
+        })
+    })
+             
+          
 }
